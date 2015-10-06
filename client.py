b85569dcd79b4ef8108809d0b2a058005e74137e
Yves Quemener <yves@iv-devs.com>
2015-10-06 16:50:20 +0900

    Now with serial-reading client

--- client.py
+++ client.py
@@ -1,11 +1,25 @@
 import requests
+import serial
 
 #url = 'http://field-test.iv-labs.org/post'
-url = 'http://localhost:4489/post'
+url = 'http://iv-labs.org:4489/post'
+#url = 'http://localhost:4489/post'
 values = {'identity' : 'bond/james/bond',
           'content' : "{'temperature':28, 'trap':0, 'water':25}"}
 
 
-r = requests.post(url, data=values)
+ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=1)
+while True:
+	s=""
+	while len(s)==0:
+		s=ser.read()
+	
+	byte=ord(s[0])
+	values = {'identity' : 'bond/james/bond',
+	          'content' : "{'temperature':"+str(byte)+", 'trap':0, 'water':25}"}
+	r = requests.post(url, data=values)
+	print r.text
+ser.close()
 
-print r
+
+
